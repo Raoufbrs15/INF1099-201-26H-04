9ab8a07a220203cc3969597eace5ce16d41107ba
Brice <300098957@collegeboreal.ca>
2026-04-08 11:07:35 -0400

    Update commons.ps1

--- .scripts/commons.ps1
+++ .scripts/commons.ps1
@@ -1,5 +1,25 @@
 #!/usr/bin/env pwsh
 
+function Num-ToEmoji {
+    param (
+        [int]$n
+    )
+
+    switch ($n) {
+        0 { ":zero:" }
+        1 { ":one:" }
+        2 { ":two:" }
+        3 { ":three:" }
+        4 { ":four:" }
+        5 { ":five:" }
+        6 { ":six:" }
+        7 { ":seven:" }
+        8 { ":eight:" }
+        9 { ":nine:" }
+        default { ":keycap_ten:" }
+    }
+}
+
 function Test-CommonItemExists {
     param(
         [string]$Path,
