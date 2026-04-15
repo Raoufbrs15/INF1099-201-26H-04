3148e9d959c32d064d46c41cc38876bb9de87e2b
jesmina Dos-reis <dosjessi4@gmail.com>
2026-04-15 14:03:10 -0400

    Update 01-ddl.sql

--- 7.PLSQL/300150303/init/01-ddl.sql
+++ 7.PLSQL/300150303/init/01-ddl.sql
@@ -23,16 +23,3 @@ CREATE TABLE logs (
     action TEXT,
     date_action TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE cours (
-    id SERIAL PRIMARY KEY,
-    nom TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE inscriptions (
-    id SERIAL PRIMARY KEY,
-    etudiant_id INT REFERENCES etudiants(id),
-    cours_id INT REFERENCES cours(id),
-    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(etudiant_id, cours_id)
-);
