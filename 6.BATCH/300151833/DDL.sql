45c7c63e2daf75503a2adca81b75d8f61dbf7b55
Raoufbrs15 <109877652+Raoufbrs15@users.noreply.github.com>
2026-04-21 19:25:32 -0400

    Create 'etudiants' table in DDL.sql
    Add DDL for creating 'etudiants' table with columns.

--- 6.BATCH/300151833/DDL.sql
+++ 6.BATCH/300151833/DDL.sql
@@ -1,1 +1,7 @@
+-- DDL.sql : Création de la structure de la base de données
 
+CREATE TABLE IF NOT EXISTS etudiants (
+    id   SERIAL PRIMARY KEY,
+    nom  VARCHAR(100) NOT NULL,
+    age  INT
+);
